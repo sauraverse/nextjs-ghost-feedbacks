42b250b00b12f5a0841a146e95da8bddf3b63ecd
sauraverse <sauravk.eth@gmail.com>
2024-07-02 15:28:27 +0530

    Frontend sign-up page built using react-hook-form & shadcn/ui

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,54 +1,8 @@
-'use client'
-import {zodResolver} from "@hookform/resolvers/zod"
-import {useForm} from "react-hook-form"
-import * as z from "zod"
-import Link from "next/link"
-import { useEffect, useState } from "react"
-import { useDebounceValue} from "usehooks-ts"
-import { useToast } from "@/components/ui/use-toast"
-import { useRouter } from "next/navigation"
-import { signUpSchema } from "@/schemas/signUpSchema"
-import axios, { AxiosError } from "axios"
-import { ApiResponse } from "@/types/ApiResponses"
+import React from 'react'
 
 const page = () => {
-  const [username, setUsername] = useState('')
-  const [usernameMessage, setUsernameMessage] = useState('')
-  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
-  const [isSubmitting,  setIsSubmitting] = useState(false);
-  
-  const debouncedUsername = useDebounceValue(username, 300)
-
-  const {toast} = useToast();
-  const router = useRouter();
-
-  //zod implementation
-  const form = useForm<z.infer<typeof signUpSchema>>({
-    resolver: zodResolver(signUpSchema),
-    defaultValues: {
-      username: '',
-      email: '',
-      password: ''
-    }
-  })
-
-  useEffect(()=> {
-    const checkUsernameUnique = async () => {
-      if(debouncedUsername){
-        setIsCheckingUsername(true)
-        setUsernameMessage('')
-        try {
-          const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`)
-          setUsernameMessage(response.data.message)
-        } catch (error) {
-          const axiosError = error as AxiosError<ApiResponse>
-          setUsernameMessage(axiosError.response?.data.message ?? 'Error checking username')
-        }
-      }
-    }
-  },[debouncedUsername])
   return (
-    <div>page</div>
+    <div>Sign-in Page</div>
   )
 }
 
