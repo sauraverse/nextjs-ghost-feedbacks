e3b0d88b8bcfda660e906187d0aae5f94ab24b04
sauraverse <sauravk.eth@gmail.com>
2024-07-25 11:00:09 +0530

    Integrated Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}  
         <Toaster />
+        <Analytics mode={'production'} />;
       </body>
       </AuthProvider>
     </html>
